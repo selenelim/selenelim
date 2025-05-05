a880e53a2510e534a033e9c0b93ec96e09bee204
selenelim <bobofishballg@gmail.com>
2025-05-05 16:36:06 +0800

    done

--- ET0735_LAB_2/Lab2.1.py
+++ ET0735_LAB_2/Lab2.1.py
@@ -1,1 +1,24 @@
 print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+
+def display_main_menu():
+    print("display_main_menu")
+    prinr("Enter some numbers seperated by commas (e.g. 5, 67, 32)")
+
+def calc_average():
+    print("calc_average")
+
+def get_user_input():
+    print("get_user_input")
+    x=input("Enter numbers seperated by commas:")
+    x_list=x.split(",")
+    float_list=[float(num.strip()) for num in x_list]
+    return float_list
+
+def find_min_max():
+    print ("find_min_max")
+
+def sort_temperature():
+    print("sort_temperature")
+
+def calc_median_temp():
+    print("calc_median_temp")
